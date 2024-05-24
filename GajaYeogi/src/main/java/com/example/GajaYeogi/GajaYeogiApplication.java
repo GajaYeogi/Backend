97021ac4cdf871f1c4b17427b3b0615e4191c734
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2024-05-24 13:49:03 +0900

    back-end

---
+++ GajaYeogi/src/main/java/com/example/GajaYeogi/GajaYeogiApplication.java
@@ -0,0 +1,13 @@
+package com.example.GajaYeogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GajaYeogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GajaYeogiApplication.class, args);
+	}
+
+}
