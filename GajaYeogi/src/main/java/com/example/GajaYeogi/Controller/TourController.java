a05b2d8ec9f9efde650b8b0d8b0510db19a731d7
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2024-06-09 14:03:51 +0900

    backend

--- GajaYeogi/src/main/java/com/example/GajaYeogi/Controller/TourController.java
+++ GajaYeogi/src/main/java/com/example/GajaYeogi/Controller/TourController.java
@@ -5,10 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/tour")
 public class TourController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TourController.class);
+
     private final TourService tourService;
 
     public TourController(TourService tourService) {
@@ -17,7 +22,11 @@ public class TourController {
 
     @GetMapping("/info")
     public ResponseEntity<String> getTourInfo() {
+        logger.info("Received request for tour info");
         String tourInfo = tourService.getTourInfo();
+        if ("Error fetching tour info".equals(tourInfo)) {
+            return ResponseEntity.status(500).body(tourInfo);
+        }
         return ResponseEntity.ok(tourInfo);
     }
 }
--- GajaYeogi/src/main/java/com/example/GajaYeogi/Service/TourService.java
+++ GajaYeogi/src/main/java/com/example/GajaYeogi/Service/TourService.java
@@ -1,42 +1,63 @@
 package com.example.GajaYeogi.Service;
 
-    import org.springframework.beans.factory.annotation.Value;
-    import org.springframework.boot.web.client.RestTemplateBuilder;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.stereotype.Service;
-    import org.springframework.web.client.RestTemplate;
-    import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-    @Service
-    public class TourService {
-        @Value("${tourapi.key}")
-        private String apiKey;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-        private final RestTemplate restTemplate;
+@Service
+public class TourService {
 
-        public TourService(RestTemplateBuilder restTemplateBuilder) {
-            this.restTemplate = restTemplateBuilder.build();
-        }
+    @Value("${tourapi.key}")
+    private String apiKey;
 
-        public String getTourInfo() {
-            String url = UriComponentsBuilder.fromHttpUrl("http://apis.data.go.kr/B551011/KorService1/locationBasedList1?")
-                    .queryParam("serviceKey", apiKey)
-                    .queryParam("pageNo", 1)
-                    .queryParam("numOfRows", 10)
-                    .queryParam("MobileOS", "ETC")
-                    .queryParam("MobileApp", "AppTest")
-                    .queryParam("_type", "json")
-                    .queryParam("mapX", 126.981611)
-                    .queryParam("mapY", 37.568477)
-                    .queryParam("radius", 1000)
-                    .queryParam("contentTypeId", 25)
-                    .build(false) // 디코딩하지 않도록 설정
-                    .toUriString();
+    private final RestTemplate restTemplate;
 
-            System.out.println(url);
-            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-            return response.getBody();
-        }
+    public TourService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
     }
 
+    public String getTourInfo() {
+        String urlRoot = "http://apis.data.go.kr/B551011/KorService1/locationBasedList1";
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(urlRoot)
+                .queryParam("serviceKey", apiKey)
+                .queryParam("pageNo", 1)
+                .queryParam("numOfRows", 10)
+                .queryParam("MobileOS", "ETC")
+                .queryParam("MobileApp", "AppTest")
+                .queryParam("_type", "json")
+                .queryParam("mapX", 126.981611)
+                .queryParam("mapY", 37.568477)
+                .queryParam("radius", 1000)
+                .queryParam("contentTypeId", 25);
 
+        URI uri;
+        try {
+            uri = new URI(uriBuilder.toUriString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Error creating URI", e);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Accept", "*/*;q=0.9");
+
+        HttpEntity<String> httpRequest = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.exchange(uri, HttpMethod.GET, httpRequest, new ParameterizedTypeReference<String>() {});
+        } catch (RestClientException e) {
+            throw new RuntimeException("Error fetching tour info", e);
+        }
+
+        return response.getBody();
+    }
+}
